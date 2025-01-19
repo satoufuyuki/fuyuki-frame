11e5109ae846becd9394bb654a45ef8cec47e848
satoufuyuki <satoufuyuki@proton.me>
2025-01-19 08:25:51 +0700

    feat(drizzle): add fallback db file

--- app/drizzle/config.server.ts
+++ app/drizzle/config.server.ts
@@ -3,8 +3,8 @@ import process from "node:process";
 import { drizzle } from "drizzle-orm/libsql";
 import { migrate } from "drizzle-orm/libsql/migrator";
 
-export const db = drizzle(process.env.DB_FILE_NAME ?? "");
+export const db = drizzle(process.env.DB_FILE_NAME ?? "file:data/database.db");
 
 void migrate(db, {
-    migrationsFolder: "app/drizzle/migrations"
+    migrationsFolder: "drizzle/migrations"
 });
