6a76aecc833b4a57f395d1522d46f7fd405bf371
satoufuyuki <satoufuyuki@proton.me>
2025-01-22 22:41:30 +0700

    fix: move drizzle migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import process from "node:process";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    out: "./app/drizzle/migrations",
+    out: "./drizzle/migrations",
     schema: "./app/drizzle/schema",
     dialect: "postgresql",
     dbCredentials: {
